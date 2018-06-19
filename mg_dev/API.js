7c059af88c2e572cbd6dc8b07b7ef99cfa46c678
Leopold Chen <placeintime.qh@gmail.com>
2018-06-19 13:42:46 +0800

    revert 后台管理

--- mg_dev/API.js
+++
@@ -1,9 +0,0 @@
-//API
-
-// const path = "http://localhost:3000";
-
-const API = {
-  postUpsertType: "/api/_mg/admin/upsertType"
-};
-
-export default API;
--- mg_dev/index.js
+++
@@ -1,6 +0,0 @@
-import "babel-polyfill";
-import React from "react";
-import ReactDOM from "react-dom";
-import BrowserApp from "./components/browserApp";
-
-ReactDOM.render(<BrowserApp />, document.getElementById("app"));
